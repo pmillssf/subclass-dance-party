8d994366b3900cb8e45b7db2cc7b7a94e12b0991
Hack Reactor Students <accounts@hackreactor.com>
2017-02-13 17:48:12 -0800

    Fully refactor Dancer and BlinkyDancer class
    Pass all tests

--- spec/blinkyDancerSpec.js
+++ spec/blinkyDancerSpec.js
@@ -5,7 +5,7 @@ describe('blinkyDancer', function() {
 
   beforeEach(function() {
     clock = sinon.useFakeTimers();
-    blinkyDancer = makeBlinkyDancer(10, 20, timeBetweenSteps);
+    blinkyDancer = new BlinkyDancer(10, 20, timeBetweenSteps);
   });
 
   it('should have a jQuery $node object', function() {
@@ -22,13 +22,15 @@ describe('blinkyDancer', function() {
     it('should call step at least once per second', function() {
       sinon.spy(blinkyDancer, 'step');
       expect(blinkyDancer.step.callCount).to.be.equal(0);
+      console.log('1');
       clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
       clock.tick(timeBetweenSteps);
-
+  
       expect(blinkyDancer.step.callCount).to.be.equal(1);
-
+      console.log('2');
       clock.tick(timeBetweenSteps);
       expect(blinkyDancer.step.callCount).to.be.equal(2);
+      console.log('3');
     });
   });
 });
--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -3,18 +3,19 @@ var BlinkyDancer = function(top, left, timeBetweenSteps) {
 
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
-
-  this.oldStep = this.step;
 };
 
 BlinkyDancer.prototype = Object.create(Dancer.prototype);
 BlinkyDancer.prototype.constructor = BlinkyDancer;
 
-BlinkyDancer.prototype.step = function() {
+BlinkyDancer.prototype.step = function(timeBetweenSteps) {
   // call the old version of step at the beginning of any call to this new version of step
-  this.oldStep();
+  this.oldStep = Dancer.prototype.step;
+  //console.log(this.oldStep);
+  this.oldStep(timeBetweenSteps);
   // toggle() is a jQuery method to show/hide the <span> tag.
   // See http://api.jquery.com/category/effects/ for this and
   // other effects you can use on a jQuery-wrapped html tag.
+  console.log(this.$node);
   this.$node.toggle();
 };
